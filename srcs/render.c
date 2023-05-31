08c93f9a891a26e735d7f2ff2246fa9b20945874
Alexis Brisse <alexisbrisse@gmail.com>
2023-06-01 00:59:10 +0200

    fix merge conflict

--- srcs/render.c
+++ srcs/render.c
@@ -6,11 +6,7 @@
 /*   By: abrisse <abrisse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 13:23:02 by abrisse           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/05/31 20:42:28 by abrisse          ###   ########.fr       */
-=======
-/*   Updated: 2023/06/01 00:49:24 by abrisse          ###   ########.fr       */
->>>>>>> add-texture
+/*   Updated: 2023/06/01 00:58:49 by abrisse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,80 +26,6 @@ void	render_background(t_data *data)
 		| data->floor[2];
 	draw_color_rect(&data->graphic.game, &data->to_draw);
 }
-
-<<<<<<< HEAD
-static void	render_wall(t_ray *ray, t_data *data)
-{
-	int texture_x;
-	int texture_y;
-	int i;
-	int top_distance;
-	
-	if (ray->was_hit_horizontal)
-		texture_x = (int)ray->hit_coord.x % TILE_SIZE;
-	else
-		texture_x = (int)ray->hit_coord.y % TILE_SIZE;
-	top_distance = (int)ray->strip.y - 1 + (int)ray->strip.height / 2 - (int)data->graphic.win_height / 2;
-	
-	i = -1;
-	while (++i < ray->strip.height)
-	{
-	
-		if (ray->was_hit_horizontal)
-		{
-			if (ray->is_facing_up)
-			{
-				texture_y = top_distance * ((float)data->graphic.north_texture.height / ray->strip.height) + i;
-				ray->strip.color = *(int*)(data->graphic.north_texture.addr + ((data->graphic.north_texture.bpp / 8) * texture_x + texture_y * data->graphic.north_texture.line_len));
-			}
-			else
-			{
-				texture_y = top_distance * ((float)data->graphic.south_texture.height / ray->strip.height) + i;
-				ray->strip.color = *(int*)(data->graphic.south_texture.addr + ((data->graphic.south_texture.bpp / 8) * texture_x + texture_y * data->graphic.south_texture.line_len));
-			}
-		}
-		else
-		{
-			if (ray->is_facing_left)
-			{
-				texture_y = top_distance * ((float)data->graphic.west_texture.height / ray->strip.height) + i;
-				ray->strip.color = *(int*)(data->graphic.west_texture.addr + ((data->graphic.west_texture.bpp / 8) * texture_x + texture_y * data->graphic.west_texture.line_len));
-			}
-			else
-			{
-				texture_y = top_distance * ((float)data->graphic.east_texture.height / ray->strip.height) + i;
-				ray->strip.color = *(int*)(data->graphic.east_texture.addr + ((data->graphic.east_texture.bpp / 8) * texture_x + texture_y * data->graphic.east_texture.line_len));
-			}
-		}
-		my_mlx_pixel_put(data->graphic.game.img, ray->strip.x, ray->strip.y + i, ray->strip.color);
-	}
-}
-
-void	render_walls(t_data *data)
-{
-	int	x;
-
-	x = -1;
-	while (++x < data->num_rays)
-	{
-		data->rays[x].wall_distance = data->rays[x].hit_distance
-			* cos(data->rays[x].angle - data->player.angle);
-		data->rays[x].strip.height = (TILE_SIZE / data->rays[x].wall_distance)
-			* data->distance_projection;
-		if (data->rays[x].strip.height > data->graphic.win_height)
-			data->rays[x].strip.height = data->graphic.win_height;
-		data->rays[x].strip.y = (data->graphic.win_height / 2)
-			- (data->rays[x].strip.height / 2);
-		if (data->rays[x].strip.y < 0)
-			data->rays[x].strip.y = 0;
-		data->rays[x].strip.width = WALL_STRIP_WIDTH;
-		data->rays[x].strip.x = data->graphic.win_width - 1 - x
-			* WALL_STRIP_WIDTH;
-		render_wall(&data->rays[x], data);
-	}
-}
-=======
->>>>>>> add-texture
 
 void	render_player(t_data *data)
 {
